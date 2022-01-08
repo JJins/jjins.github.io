5aa98b96ed274fa1da19f31477c9bce6c682dece
JJins <ljsh093@gmail.com>
2022-01-08 17:24:29 +0800

    Update fstock.py

--- t2i/fstock.py
+++ t2i/fstock.py
@@ -21,28 +21,28 @@ for stock in resObj["stocks"]["mex"]["stocks"]:
         mexStock["hua"] = stock["quantity"]
     if "Jaguar Plushie" == stock["name"]:
         mexStock["ou"] = stock["quantity"]
-seq.append("墨 花%s 偶%s\n" % (mexStock["hua"], mexStock["ou"]))
+seq.append("墨  花%s 偶%s\n" % (mexStock["hua"], mexStock["ou"]))
 cayStock = {}
 for stock in resObj["stocks"]["cay"]["stocks"]:
     if "Banana Orchid" == stock["name"]:
         cayStock["hua"] = stock["quantity"]
     if "Stingray Plushie" == stock["name"]:
         cayStock["ou"] = stock["quantity"]
-seq.append("开 花%s 偶%s\n" % (cayStock["hua"], cayStock["ou"]))
+seq.append("开  花%s 偶%s\n" % (cayStock["hua"], cayStock["ou"]))
 canStock = {}
 for stock in resObj["stocks"]["can"]["stocks"]:
     if "Crocus" == stock["name"]:
         canStock["hua"] = stock["quantity"]
     if "Wolverine Plushie" == stock["name"]:
         canStock["ou"] = stock["quantity"]
-seq.append("加 花%s 偶%s\n" % (canStock["hua"], canStock["ou"]))
+seq.append("加  花%s 偶%s\n" % (canStock["hua"], canStock["ou"]))
 hawStock = {}
 for stock in resObj["stocks"]["haw"]["stocks"]:
     if "Orchid" == stock["name"]:
         hawStock["hua"] = stock["quantity"]
     if "Large Suitcase" == stock["name"]:
         hawStock["xiang"] = stock["quantity"]
-seq.append("夏 花%s 箱%s\n" % (hawStock["hua"], hawStock["xiang"]))
+seq.append("夏  花%s 箱%s\n" % (hawStock["hua"], hawStock["xiang"]))
 ukStock = {}
 for stock in resObj["stocks"]["uni"]["stocks"]:
     if "Red Fox Plushie" == stock["name"]:
@@ -51,7 +51,7 @@ for stock in resObj["stocks"]["uni"]["stocks"]:
         ukStock["Nessie"] = stock["quantity"]
     if "Heather" == stock["name"]:
         ukStock["Heather"] = stock["quantity"]
-seq.append("嘤 赤狐%s 水怪%s 花%s\n" % (ukStock["Red"], ukStock["Nessie"], ukStock["Heather"]))
+seq.append("嘤  赤狐%s 水怪%s 花%s\n" % (ukStock["Red"], ukStock["Nessie"], ukStock["Heather"]))
 agtStock = {}
 for stock in resObj["stocks"]["arg"]["stocks"]:
     if "Ceibo Flower" == stock["name"]:
@@ -60,33 +60,33 @@ for stock in resObj["stocks"]["arg"]["stocks"]:
         agtStock["ou"] = stock["quantity"]
     if "Tear Gas" == stock["name"]:
         agtStock["tear"] = stock["quantity"]
-seq.append("阿 花%s 偶%s 雷%s\n" % (agtStock["hua"], agtStock["ou"], agtStock["tear"]))
+seq.append("阿  花%s 偶%s 雷%s\n" % (agtStock["hua"], agtStock["ou"], agtStock["tear"]))
 swiStock = {}
 for stock in resObj["stocks"]["swi"]["stocks"]:
     if "Chamois Plushie" == stock["name"]:
         swiStock["ou"] = stock["quantity"]
     if "Edelweiss" == stock["name"]:
         swiStock["hua"] = stock["quantity"]
-seq.append("瑞 花%s 偶%s\n" % (swiStock["hua"], swiStock["ou"]))
+seq.append("瑞  花%s 偶%s\n" % (swiStock["hua"], swiStock["ou"]))
 jpStock = {}
 for stock in resObj["stocks"]["jap"]["stocks"]:
     if "Cherry Blossom" == stock["name"]:
         jpStock["hua"] = stock["quantity"]
-seq.append("日 花%s\n" % jpStock["hua"])
+seq.append("日  花%s\n" % jpStock["hua"])
 zgStock = {}
 for stock in resObj["stocks"]["chi"]["stocks"]:
     if "Panda Plushie" == stock["name"]:
         zgStock["ou"] = stock["quantity"]
     if "Peony" == stock["name"]:
         zgStock["hua"] = stock["quantity"]
-seq.append("中 花%s 偶%s\n" % (zgStock["hua"], zgStock["ou"]))
+seq.append("中  花%s 偶%s\n" % (zgStock["hua"], zgStock["ou"]))
 uaeStock = {}
 for stock in resObj["stocks"]["uae"]["stocks"]:
     if "Camel Plushie" == stock["name"]:
         uaeStock["ou"] = stock["quantity"]
     if "Tribulus Omanense" == stock["name"]:
         uaeStock["hua"] = stock["quantity"]
-seq.append("迪 花%s 偶%s\n" % (uaeStock["hua"], uaeStock["ou"]))
+seq.append("迪  花%s 偶%s\n" % (uaeStock["hua"], uaeStock["ou"]))
 nfStock = {}
 for stock in resObj["stocks"]["sou"]["stocks"]:
     if "Lion Plushie" == stock["name"]:
@@ -95,7 +95,7 @@ for stock in resObj["stocks"]["sou"]["stocks"]:
         nfStock["hua"] = stock["quantity"]
     if "Xanax" == stock["name"]:
         nfStock["xan"] = stock["quantity"]
-seq.append("南 花%s 偶%s XAN%s" % (nfStock["hua"], nfStock["ou"], nfStock["xan"]))
+seq.append("南  花%s 偶%s XAN%s" % (nfStock["hua"], nfStock["ou"], nfStock["xan"]))
 
 fo = open(file="stock.txt", mode="w", encoding="utf-8")
 fo.writelines(seq)
